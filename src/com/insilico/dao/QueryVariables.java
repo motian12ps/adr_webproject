4f5130bb92932106691cc5d0a05ba8e3c6003b5f
motian12ps <chentianyi@ChenTianyis-MacBook-Pro-2.local>
2015-06-02 23:04:36 -0400

    web

---
+++ src/com/insilico/dao/QueryVariables.java
@@ -0,0 +1,27 @@
+package com.insilico.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QueryVariables {
+	
+	public static List<Integer> year=new ArrayList<Integer>();
+	public static List<String> quarter=new ArrayList<String>();
+	
+	public static String measure=null;
+	
+	public static List<String> gender=new ArrayList<String>();
+	
+	public static List<Integer> age=new ArrayList<Integer>();
+	
+	public static List<Double> weight=new ArrayList<Double>();
+	
+	public static List<String> country=new ArrayList<String>();
+	
+	public static int numOutput=5; //default K=5 to output the top 5 most likely relationship
+	
+	
+	
+	
+	
+}
